4e739fbdfbfcfff211b409b5793e22edbd108850
Nilang <nlg5197@gmail.com>
2025-05-18 17:29:25 +0530

    Sign-up UI complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -9,6 +10,12 @@ import authRoutes from "./routes/auth.route.js";
 import MessageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
